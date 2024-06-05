1edb00c279caaf504613fbdefba94a15a4d15aa1
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-05 20:15:59 +0600

    Initialize savil/fix-dev-build

---
+++ envsec/pkg/envsec/auth.go
@@ -0,0 +1,67 @@
+package envsec
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"go.jetpack.io/pkg/api"
+	"go.jetpack.io/pkg/auth"
+)
+
+func (e *Envsec) authClient() (*auth.Client, error) {
+	return auth.NewClient(
+		e.Auth.Issuer,
+		e.Auth.ClientID,
+		[]string{"openid", "offline_access", "email", "profile"},
+	)
+}
+
+func (e *Envsec) WhoAmI(
+	ctx context.Context,
+	w io.Writer,
+	showTokens bool,
+) error {
+	authClient, err := e.authClient()
+	if err != nil {
+		return err
+	}
+
+	tok, err := authClient.GetSession(ctx)
+	if err != nil {
+		return fmt.Errorf("error: %w, run `envsec auth login`", err)
+	}
+
+	idClaims := tok.IDClaims()
+
+	fmt.Fprintf(w, "Logged in\n")
+	fmt.Fprintf(w, "User ID: %s\n", idClaims.Subject)
+
+	if idClaims.OrgID != "" {
+		fmt.Fprintf(w, "Org ID: %s\n", idClaims.OrgID)
+	}
+
+	if idClaims.Email != "" {
+		fmt.Fprintf(w, "Email: %s\n", idClaims.Email)
+	}
+
+	if idClaims.Name != "" {
+		fmt.Fprintf(w, "Name: %s\n", idClaims.Name)
+	}
+
+	apiClient := api.NewClient(ctx, e.APIHost, tok)
+
+	member, err := apiClient.GetMember(ctx, tok.IDClaims().Subject)
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "Org name: %s\n", member.Organization.Name)
+
+	if showTokens {
+		fmt.Fprintf(w, "Access Token: %s\n", tok.AccessToken)
+		fmt.Fprintf(w, "ID Token: %s\n", tok.IDToken)
+		fmt.Fprintf(w, "Refresh Token: %s\n", tok.RefreshToken)
+	}
+	return nil
+}
---
+++ typeid/typeid/go.mod
@@ -0,0 +1,14 @@
+module go.jetpack.io/typeid-cli
+
+go 1.20
+
+require (
+	github.com/spf13/cobra v1.8.0
+	go.jetpack.io/typeid v1.0.0
+)
+
+require (
+	github.com/gofrs/uuid/v5 v5.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
